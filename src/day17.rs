bc6b7c8c4b13afd409730044f0ad17a83ede1b59
Sam Davies <samgdavies@gmail.com>
2023-03-17 20:59:41 +0000

    17 part two - efficient version

--- src/day17.rs
+++ src/day17.rs
@@ -1,11 +1,11 @@
 use std::collections::VecDeque;
 
 pub fn solve() {
-    let steps = 377;
+    const STEPS: usize = 377;
     let mut buff = VecDeque::<u32>::new();
     buff.push_back(0);
     for x in 1..(2017 + 1) {
-        let step = steps % buff.len();
+        let step = STEPS % buff.len();
         // move so that selection is at end of vec
         // [..., new_s, ..., old_s] -> [..., os, ..., ns] -> [..., os, ..., ns, x]
         // because we always start at the end, new_s is at [step]
@@ -18,30 +18,18 @@ pub fn solve() {
     println!("17 part one {}", buff.pop_front().unwrap());
 
     // part two - 50_000_000 iterations
-    // can't keep adding in middle of array (& resizing) -- too slow
-    // Instead use linked list.  Implement as array[entry] = next.
-    const LIM: usize = 50_000_000;
-    let mut abuff = Vec::<usize>::with_capacity(LIM + 1); // Too large to alloc on stack
-    for _ in 0..LIM + 1 {
-        abuff.push(2 * LIM); // Seed with invalid value
-    }
-    let mut abuff = abuff.into_boxed_slice();
-    abuff[0] = 1;
-    abuff[1] = 0;
-
-    let mut current = 1;
-    for x in 2..LIM + 1 {
-        for _ in 0..steps {
-            current = abuff[current];
+    // We only need to know the value immediately after 0 - so just keep track
+    // of when we would have inserted an item at (i.e. after) position 0.
+    let mut after0 = 0;
+    let mut length = 1;
+    let mut pos = 0;
+    for i in 1..50_000_000+1 {
+        pos = (pos+STEPS) % length;
+        if pos==0 {
+            after0 = i;
         }
-        abuff[x] = abuff[current];
-        abuff[current] = x;
-        current = x;
-
-        if x == 2017 {
-            println!("... {}", abuff[x]); // part one
-        }
+        pos += 1; // Inserted value becomes new position
+        length += 1;
     }
-
-    println!("27 part two {}", abuff[0]);
+    println!("17 part two {}", after0);
 }
